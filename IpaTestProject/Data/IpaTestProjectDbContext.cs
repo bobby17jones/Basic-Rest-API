3bda99846e1e51ca617f7b90c83675856a4b8672
Raphael Krähenbühl <raphael.kraehenbuehl@bucher-suter.com>
2023-01-20 15:17:03 +0100

    Add project files.

---
+++ IpaTestProject/Data/IpaTestProjectDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IpaTestProject.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace IpaTestProject.Data
+{
+    public class IpaTestProjectDbContext : DbContext
+    {
+        public IpaTestProjectDbContext(DbContextOptions<IpaTestProjectDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
